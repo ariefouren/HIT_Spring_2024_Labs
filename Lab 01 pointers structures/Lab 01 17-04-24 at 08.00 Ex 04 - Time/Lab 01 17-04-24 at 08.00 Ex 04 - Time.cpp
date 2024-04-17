b1851a33cb159c5dcf1ef6b13a2c00ecfd48504e
ariefouren <aporan@ono.ac.il>
2024-04-17 09:12:15 +0300

    17-04-24

---
+++ Lab 01 pointers structures/Lab 01 17-04-24 at 08.00 Ex 04 - Time/Lab 01 17-04-24 at 08.00 Ex 04 - Time.cpp
@@ -0,0 +1,26 @@
+
+
+// define constants
+
+// define a structure to represent time
+
+// the function converts the time value to a standard form
+void standardize(???)
+
+// the function inputTimeArray reads a sequence of time values, 
+// stores them in an array of structures and then standardizes
+// the function returns a pointer to the array 
+// of the canonicalized time values
+Time* inputTimeArray
+
+// the function prints the array of time values 
+// in the standard form
+void printTimeArray( ??? )
+
+int main()
+{
+    int n = 0;
+    Time* times = inputTimeArray(n);
+    printTimeArray(times, n);
+}
+
